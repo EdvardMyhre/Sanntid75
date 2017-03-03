497ee5064d77086842adf15c28bc1a317d6c7ac1
Morten Lykkedrang <morten.lykkedrang@gmail.com>
2017-03-03 11:36:10 +0100

    Add task distributor file

---
+++ temp/task_distributor.go
@@ -0,0 +1,52 @@
+
+package main
+import (
+	"fmt"
+	"time"
+)
+
+func main(){
+	task_distributor()
+	//fmt.Println("main function. Connectivity test")
+}
+
+func task_distributor() {
+	fmt.Println("task distributor, Connectivity test")
+	const (
+		waiting_for_task = 1		//001
+		waiting_for_response = 2	//010
+		send_choice = 5				//100
+									//111
+	)
+	var task_distributor_state int = waiting_for_task
+	task_dist_timestamp := time.Now()
+	fmt.Println("Timestamp: ",task_dist_timestamp)
+	
+	
+	for {
+		switch task_distributor_state{
+		case waiting_for_task:
+			//Waits for new task from task_manager
+			//fmt.Println("Waiting for task")
+			task_distributor_state = waiting_for_response
+		case waiting_for_response:
+			//Waits for other nodes to respond
+			//fmt.Println("Waiting for response")
+			task_distributor_state = 0
+			
+			
+		case send_choice:
+			//Sends message containing the node chosen for task
+			
+			
+			
+		default:
+			task_distributor_state = waiting_for_task	
+			fmt.Println("Default state")
+			time.Sleep(2*time.Second)
+			fmt.Println("Time difference: ",time.Since(task_dist_timestamp).Seconds())
+			
+		}
+	}
+	
+}
