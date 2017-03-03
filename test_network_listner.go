8598ff3ad716e8e76f07b91afeb5d3806dc9a24b
Lars Erik <larserikskraastad@hotmail.com>
2017-03-03 15:59:03 +0100

    endret navn på testfunskjon

---
+++ test_network_listner.go
@@ -0,0 +1,42 @@
+
+
+//-----------------------  Heis 1  ----------------
+
+
+package main 
+ 
+import (
+    //"fmt"
+    //"net"
+    "time"
+    "./Network"
+)
+
+
+
+
+ 
+func main() {
+
+    a,_ := Network.LocalIP()
+
+    send_objekt := Network.MainData{}
+    send_objekt.Source = a
+    send_objekt.Destination = "heis 2"
+    //b, err := json.Marshal(data)
+    send_objekt.Message_type = 3
+    row1 := []int{1,2,3,4,52}
+    row2 := []int{4,5,6,564,4}
+    send_objekt.Data = append(send_objekt.Data,row1)
+    send_objekt.Data = append(send_objekt.Data,row2)
+    
+
+    Network.Udp_listner(10001)
+
+/*
+    for i := 0; i < 6; i++ {
+        Network.Udp_broadcast(send_objekt, 10001)
+        time.Sleep(time.Second * 1)
+    }*/
+
+}
