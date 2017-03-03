c66efbdd58ba0df7e9dfbf08d4542337677c13bf
aestene <steneae@gmail.com>
2017-03-03 12:55:14 +0100

    changed structs

--- Network/structer.go
+++ Network/structer.go
@@ -1,21 +1,16 @@
-
 package Network
- 
 
-
-
-type Commands struct { // Dette er dataen til elevator_handler som lagres i commands
+type Command struct { // Dette er dataen til elevator_handler som lagres i commands
     Next_floor int
     Current_floor int
     Person_inside bool
-
 }
 
-type NewOrder struct { // Dataen som kommer fra knappene
-    Floor int
-    Direction int
+type Button struct { // Data from buttons are stored here
+    Elev_id int
+    Button_type int
+    Floor int		
 }
-
 
 type MainData struct { // Dette som sendes mellom heisene og legges i backoup
     Source string
--- driver/input_intermediary.go
+++ driver/input_intermediary.go
@@ -1,18 +1,29 @@
-package main
+package main  // where "driver" is the folder that contains io.go, io.c, io.h, channels.go, channels.h and driver.go
 /*
-#include<stdio.h>
-
-hello()
-{
-    return 1;
-}
+#cgo CFLAGS: -std=c11
+#cgo LDFLAGS: -lcomedi -lm
+#include "io.h"
 */
-import "C"
 import(
+	"C"
 	"fmt"
+	"../Network"
 )
 
-func main() {
-	fmt.Println(C.hello())
+//func Elev_get_button_signal(b Button){
+
+//}
+
+var b = Network.Button{}
+
+
+func main(){
+	b{
+		Elev_id = 1
+    	Button_type = 0
+    	Floor 1	
+	}
+	
 	
 }
+
