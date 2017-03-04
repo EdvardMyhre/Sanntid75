39c1198d78ac6a58703846c230ecf7cb78087405
Lars Erik <larserikskraastad@hotmail.com>
2017-03-04 12:53:26 +0100

    ldkfjwsdlk

--- test_network_broadcaster.go
+++ test_network_broadcaster.go
@@ -19,8 +19,7 @@ func main() {
 	row2 := []int{4, 5, 6, 564, 4}
 	send_objekt.Data = append(send_objekt.Data, row1)
 	send_objekt.Data = append(send_objekt.Data, row2)
-
-	//go Network.Udp_listner(port)
+   
 
 	for i := 0; i < 60; i++ {
 		Network.Udp_broadcast(send_objekt, port)
