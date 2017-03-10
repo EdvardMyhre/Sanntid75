5c01688cd8a602b6f19fdab5293468c8d9a45201
aestene <steneae@gmail.com>
2017-03-10 09:28:46 +0100

    klsjdf

--- main.go
+++ main.go
@@ -3,18 +3,17 @@ package main
 import (
 	"./NetworkModul"
 	"./NetworkModul/network/structer"
-	"fmt"
+	//"fmt"
 )
 
 func main() {
 	network_to_distributing_state_machine := make(chan structer.MainData)
 	go network.Network_start(network_to_distributing_state_machine)
-	for {
-
+	 for {
 		select {
-		case p := <-network_to_distributing_state_machine:
-			fmt.Println("Sendt fra main:  ", p)
-		}
-	}
+		case <-network_to_distributing_state_machine:
+	 		//fmt.Println("Sendt fra main:  ", p)
+	 	}
+	 }
 
 }
