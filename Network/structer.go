73ee8e0e48d798af6b77e082d0a65472614c53ad
Student <student@itk-d650>
2017-03-14 22:07:23 +0100

    Merged develop and network2

--- Network/structer.go
+++
@@ -1,8 +0,0 @@
-package Network
-
-type MainData struct { // Dette som sendes mellom heisene og legges i backoup
-	Source       string
-	Destination  string
-	Message_type int
-	Data         [][]int
-}
