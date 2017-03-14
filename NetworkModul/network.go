09f3da7d05d2317db0eac7214ccb237f1ee911a9
Lars Erik <larserikskraastad@hotmail.com>
2017-03-14 13:23:30 +0100

    debugg

--- NetworkModul/network.go
+++ NetworkModul/network.go
@@ -11,7 +11,7 @@ import (
 	"os"
 	//"sort"
 	"math/rand"
-	"time"
+	//"time"
 )
 
 func Network_start(n_to_distri chan structer.MainData, n_to_p_task_manager chan structer.MainData, n_to_a_tasks_manager chan structer.MainData,
@@ -82,7 +82,7 @@ func Network_start(n_to_distri chan structer.MainData, n_to_p_task_manager chan 
 				}
 
 				//my_backup_is_gone(&myBackupAlive, backupFor, p, &myBackupId)
-				time.Sleep(1000 * time.Millisecond)
+				//time.Sleep(1000 * time.Millisecond)
 				fmt.Println("Min id er:              ", id)
 				fmt.Println("Min backupid er:        ", myBackupId)
 				fmt.Println("Min myBackupAlive er:   ", myBackupAlive)
